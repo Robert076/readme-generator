0d2ae2a1a11151fb2e95c48a4268614c9f8bbe32
Robert076 <robertberes06@gmail.com>
2025-05-28 14:13:13 +0300

    description block working, also cleaned main

--- cmd/main.go
+++ cmd/main.go
@@ -1,24 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"log"
-
-	"github.com/Robert076/readme-generator/internal/blocks/title"
-)
-
 func main() {
-	titleBlock, err := title.NewTitleBlock("../internal/blocks/title/title1.tmpl")
 
-	if err != nil {
-		log.Fatal("Failed loading template:", err)
-	}
-
-	titleString, err := titleBlock.Render(struct{ ProjectTitle string }{ProjectTitle: "Test"})
-
-	if err != nil {
-		log.Fatal("Failed loading template:", err)
-	}
-
-	fmt.Print(titleString)
 }
